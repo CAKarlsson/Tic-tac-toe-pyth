9d3d540ba3a266568717caf681fd6fe7bc84961f
CAKarlsson <crisis306@hotmail.com>
2022-03-26 19:41:12 +0000

    Add function to run game, welcome message and game instructions

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def run_game():
+
+    """
+    Runs the game
+    """
+
+print("\nWelcome to tic-tac-toe!\n\n")
+print("How to play: \n")
+print("You will each take a turn to place your mark (X or O) on the board. ")
+print("Whoever manages to place three marks in a row is the winner.")
+print("These underscores is the board positions.")
+print("Top left = 1 and bottom right = 9. Pick a number to place your mark.")
+print("\n_ _ _")
+print("_ _ _")
+print("_ _ _\n")
+run_game()
