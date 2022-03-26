25ecd7c41c4d18d1945dee20f5586cba15a4e432
CAKarlsson <crisis306@hotmail.com>
2022-03-26 20:24:15 +0000

    Add function to display board with prints

--- run.py
+++ run.py
@@ -1,9 +1,34 @@
+# ------- Global Variables -------
+
+board = {0: "-", 1: "-", 2: "-",
+         3: "-", 4: "-", 5: "-",
+         6: "-", 7: "-", 8: "-"}
+
+game_running = True
+
+current_player = "X"
+
+# ------ Functions --------
 def run_game():
 
     """
     Runs the game
     """
 
+
+def display_board(board):
+    """
+    Display board
+    """
+    print(board[0] + " | " + board[1] + " | " + board[2])
+    print(board[3] + " | " + board[4] + " | " + board[5])
+    print(board[6] + " | " + board[7] + " | " + board[8])
+    print("\n")
+
+
+"""
+Welcome message
+"""
 print("\nWelcome to tic-tac-toe!\n\n")
 print("How to play: \n")
 print("You will each take a turn to place your mark (X or O) on the board. ")
